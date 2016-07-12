ea5649f822e0eb2ef78ae2d0bac492b851fd5a30
Eliot <2279986123@qq.com>
2016-07-12 21:27:00 +0800

    更新了底部条能在每次关闭后，再次打开时能够读取关闭时音乐的功能，添加了详细的注释说明

--- app/src/main/java/com/example/qiang/musicseekbar/MainActivity.java
+++ app/src/main/java/com/example/qiang/musicseekbar/MainActivity.java
@@ -47,167 +47,7 @@ import com.example.qiang.musicseekbar.util.AlbumDealUtil;
 import com.example.qiang.musicseekbar.util.DBUtil;
 
 public class MainActivity extends AppCompatActivity {
-//
-//
-//
-//    //横向滚动条
-//    private ImageView mTabline;
-//    private int mScreendeliver;
-//
-//    private ViewPager mViewPager;
-//    private ArrayList fragments;
-//    private ImageView view1, view2, view3;
-//    private int currIndex;
-//
-//    private static int bmpW;//横线图片宽度
-//    private static int offset;//图片移动的偏移量
-//
-//    //定义是否退出
-//    private boolean isExit;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-////        mTabline = (ImageView) findViewById(R.id.id_tab_line);
-//        Display display = getWindow().getWindowManager().getDefaultDisplay();
-//        DisplayMetrics outMetrics = new DisplayMetrics();
-//        display.getMetrics(outMetrics);
-//        mScreendeliver = outMetrics.widthPixels / 3;
-//        ViewGroup.LayoutParams lp = mTabline.getLayoutParams();
-//        lp.width = mScreendeliver;
-//        mTabline.setLayoutParams(lp);
-//
-//        initViewPager();
-//
-//    }
-//
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//
-//    //内部类 重写TextView点击事件
-//    public class txtListener implements View.OnClickListener {
-//        private int index = 0;
-//
-//        public txtListener(int i) {
-//            index = i;
-//        }
-//
-//        @Override
-//        public void onClick(View v) {
-//            mViewPager.setCurrentItem(index);
-//        }
-//    }
-//
-//    private void initViewPager() {
-//        //fragments页面定义、绑定等操作
-////        mViewPager = (ViewPager) findViewById(R.id.myViewPager);
-//
-//        fragments = new ArrayList<Fragment>();
-//        Fragment Fragment_Music_List = new Fragment_Music_List();
-//        Fragment Fragment_Music_Detail = new Fragment_Music_Detail();
-//
-//        fragments.add(Fragment_Music_List);
-//        fragments.add(Fragment_Music_Detail);
-//
-////        mViewPager.setAdapter(new MusicAdapter(getSupportFragmentManager(), fragments));
-//        mViewPager.setCurrentItem(0);
-//        mViewPager.setOnPageChangeListener(new myOnPageChangeListener());
-//    }
-//
-//    public class myOnPageChangeListener implements ViewPager.OnPageChangeListener {
-//        private int one = offset * 2 + bmpW;//两个相邻页面的偏移量
-//
-//        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-//        @Override
-//        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//            //position 页面位置 ;positionOffsetPixels 像素位置
-//            mTabline.setTranslationX((positionOffset + position) * mScreendeliver);
-//        }
-//
-//        @Override
-//        public void onPageScrollStateChanged(int arg0) {
-//            // TODO Auto-generated method stub
-//        }
-//
-//        @Override
-//        public void onPageSelected(int position) {
-//            // TODO Auto-generated method stub
-//            Animation animation = new TranslateAnimation(currIndex * one, position * one, 0, 0);//平移动画
-//            currIndex = position;
-//            animation.setFillAfter(true);//动画终止时停留在最后一帧，不然会回到没有执行前的状态
-//            animation.setDuration(500);//动画持续时间0.2秒
-//            //重置所欲TextView
-//            resetTextView();
-//            //给所选TextView增加颜色
-////            switch (position) {
-////                case 0:
-////                    view1.setBackgroundColor(Color.parseColor("#E0E0E0"));
-////                    break;
-////                case 1:
-////                    view2.setBackgroundColor(Color.parseColor("#E0E0E0"));
-////                    break;
-////                case 2:
-////                    view3.setBackgroundColor(Color.parseColor("#E0E0E0"));
-////                    break;
-////            }
-//
-//        }
-//    }
-//
-//    //重置颜色
-//    private void resetTextView() {
-//
-//    }
-//
-//    //重写onkeydown方法
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode == KeyEvent.KEYCODE_BACK) {
-//            exit();
-//            return false;
-//        } else {
-//            return super.onKeyDown(keyCode, event);
-//        }
-//    }
-//
-//    //定义exit退出方法
-//    public void exit() {
-//        if (!isExit) {
-//            isExit = true;
-//            Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
-//            mHandler.sendEmptyMessageDelayed(0, 2000);
-//        } else {
-//            Intent intent = new Intent(Intent.ACTION_MAIN);
-//            intent.addCategory(Intent.CATEGORY_HOME);
-//            startActivity(intent);
-//            System.exit(0);
-//        }
-//    }
-//
-//    //配置handler
-//    Handler mHandler = new Handler() {
-//        @Override
-//        public void handleMessage(Message msg) {
-//            // TODO Auto-generated method stub
-//            super.handleMessage(msg);
-//            isExit = false;
-//        }
-//
-//    };
 
-
-    private Button button1;
-    private Button button2;
     private Button buttonnext, buttonlast;
 
     private ImageButton buttonstart;
@@ -227,9 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
     List<Map<String, Object>> mlist = new ArrayList<Map<String, Object>>();
 
-    private File sdDir;
-    private String spath;
-
     private int songIndex = 0;
 
 
@@ -238,11 +75,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //定义一个新的MediaPlayer
         player = new MediaPlayer();
-
-        loadconfig();
-        init();
+        //绑定界面id与activity
         findView();
+        //读取配置文件以及初始化操作
+        init();
+        //activity中的按键控制
         setListener();
 
 
@@ -250,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onDestroy() {
         super.onDestroy();
+        //销毁程序时候的操作
         //指定操作的文件名称
 //        SharedPreferences share = getSharedPreferences("config", MODE_PRIVATE);
 //        SharedPreferences.Editor edit = share.edit(); //编辑文件
@@ -259,29 +99,42 @@ public class MainActivity extends AppCompatActivity {
         Log.i("======", "destroy");
     }
 
-    //读取配置文件
-    private void loadconfig() {
-        SharedPreferences share = getSharedPreferences("config", MODE_PRIVATE);
-        //歌曲url
-        String url = share.getString("ulr", null);
-    }
-
-
     private void init() {
-//        player = MediaPlayer.create(this, R.raw.hello);
+        //
         try {
             player.reset();
             SharedPreferences share = getSharedPreferences("config", MODE_PRIVATE);
-            //歌曲url
+            //读取配置文件中的信息，url地址、曲名、歌曲时间、专辑图像显示
             String url = share.getString("url", null);
-
+            String title = share.getString("title", null);
+            String images = share.getString("images", null);
+            String time = share.getString("time", null);
+            //判断配置文件中是否存在URL地址
             if (url != null) {
+                //如果存在，取出URL地址
                 player.setDataSource(url);
                 player.prepare();
-                Log.i("====", url);
+                //设置歌曲名
+                bottom_title.setText(title);
+                if (images == null) {
+                    //如果不存在专辑图片，使用默认图片
+                    main_layout.setBackgroundResource(R.drawable.natoli);
+                    bottom_img.setImageResource(R.drawable.natoli);
+                } else {
+                    //存在专辑图片，使用bitmapfactory进行转换
+                    AlbumDealUtil adu = new AlbumDealUtil();
+                    bm = BitmapFactory.decodeFile(images);
+                    Bitmap abm = bm;
+                    Bitmap nbm = adu.blurBitmap(bm, MainActivity.this);
+                    BitmapDrawable bmpDraw = new BitmapDrawable(nbm);
+                    main_layout.setBackgroundDrawable(bmpDraw);
+                    bottom_img.setImageBitmap(abm);
+                }
+                //设置播放时间
+                edTime.setText(time);
+                seekBar1.setMax(player.getDuration());
             } else {
-//                player.setDataSource("/sdcard/hello.mp3");
-//                player.prepare();
+                //Do nothing
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -291,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findView() {
-
+        //绑定
         buttonstart = (ImageButton) findViewById(R.id.play_button);
         opTime = (TextView) findViewById(R.id.opTime);
         edTime = (TextView) findViewById(R.id.edTime);
@@ -299,23 +152,16 @@ public class MainActivity extends AppCompatActivity {
         bottom_img = (ImageView) findViewById(R.id.bottom_bar_img);
         bottom_title = (TextView) findViewById(R.id.bottom_bar_title);
 
-
         seekBar1 = (SeekBar) findViewById(R.id.seekbar1);
-        //获得歌曲的长度并设置成播放进度条的最大值
-
-
-//        mlistview = new ListView(this);
-//        mlistview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, musicrs()));
-//        setContentView(mlistview);
-
 
         mlistview = (ListView) findViewById(R.id.music_list);
-
+        //listview数据的读取
         mlist = DBUtil.BaseMusicList(this);
         mlistview.setAdapter(new MusicAdapter(this, mlist));
 
     }
 
+    //进度条的控制
     Handler handler = new Handler();
     Runnable updateThread = new Runnable() {
         public void run() {
@@ -352,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
         player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-//播放完成后，自动切换下一首
+                //播放完成后，自动切换下一首
                 if (songIndex < mlist.size() - 1) {
                     songIndex = songIndex + 1;
                     songplay(songIndex);
@@ -369,15 +215,16 @@ public class MainActivity extends AppCompatActivity {
         mlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //播放点击的歌曲
                 songplay(position);
             }
         });
 
+        //播放和暂停的切换
         buttonstart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ISPLAY == false) {
-
                     player.start();
                     //启动
                     handler.post(updateThread);
@@ -392,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //seekbar的控制
         seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
@@ -417,17 +264,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //每次播放时候的操作
     private void songplay(int position) {
 
         songIndex = position;
+        //读取数据库中歌曲的信息
         HashMap<String, Object> geturl = (HashMap<String, Object>) mlist.get(position);
         String murl = geturl.get("url").toString();
         String back_img = geturl.get("images").toString();
         String mtitle = geturl.get("title").toString();
-
+        String mtime = geturl.get("time").toString();
+        //每次播放更新配置表中的配置数据
         SharedPreferences share = getSharedPreferences("config", MODE_PRIVATE);
         SharedPreferences.Editor edit = share.edit(); //编辑文件
         edit.putString("url", murl);
+        edit.putString("title", mtitle);
+        edit.putString("images", back_img);
+        edit.putString("time", mtime);
         edit.commit();  //保存数据信息
 
         //重置MediaPlayer状态
